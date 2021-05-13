9309aec94a9ca85cf014abce600391dbb56a8743
Matt Morrison <matt.morrison@zapier.com>
2021-05-14 09:20:34 +1200

    Prepare for release

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
+)
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: opslevel.Provider})
