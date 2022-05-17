846291c9037ae2dc4dd0dd4265e4d756e0de12f6
Edgar Ochoa <edgar@opslevel.com>
2022-05-16 20:43:04 -0500

    Fix identation

--- examples/data-sources/opslevel_group/data-source.tf
+++ examples/data-sources/opslevel_group/data-source.tf
@@ -1,7 +1,7 @@
 data "opslevel_group" "security" {
-   identifier = "security"
+    identifier = "security"
 }
 
 data "opslevel_group" "sre" {
-  identifier = "Z2lkOi8vb3BzbGV2ZWwvTmFtZXNwYWNlczo6R3JvdXAvNTI"
+    identifier = "Z2lkOi8vb3BzbGV2ZWwvTmFtZXNwYWNlczo6R3JvdXAvNTI"
 }
