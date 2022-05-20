36223411dfd8d9d856c77aea9dc74a5a7a0c8bf5
Kyle Rockman <rocktavious@icloud.com>
2022-05-20 16:16:47 -0500

    fix example for alert source usage

--- examples/resources/opslevel_check_alert_source_usage/resource.tf
+++ examples/resources/opslevel_check_alert_source_usage/resource.tf
@@ -28,16 +28,11 @@ resource "opslevel_check_alert_source_usage" "example" {
   enabled = true
   # To set a future enable date remove field 'enabled' and use 'enable_on'
   # enable_on = "2022-05-23T14:14:18.782000Z"
-  category        = data.opslevel_rubric_category.security.id
-  level           = data.opslevel_rubric_level.bronze.id
-  owner           = data.opslevel_team.devs.id
-  filter          = data.opslevel_filter.tier1.id
-  file_extensions = ["sbt", "py"]
-  file_contents_predicate {
-    type  = "contains"
-    value = "postgres"
-  }
-  notes = "Optional additional info on why this check is run or how to fix it"
+  category = data.opslevel_rubric_category.security.id
+  level    = data.opslevel_rubric_level.bronze.id
+  owner    = data.opslevel_team.devs.id
+  filter   = data.opslevel_filter.tier1.id
+  notes    = "Optional additional info on why this check is run or how to fix it"
 
   alert_type = "pagerduty" # one of: "pagerduty", "datadog", "opsgenie"
   alert_name_predicate = {
