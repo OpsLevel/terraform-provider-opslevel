05d42348ef43fd3c909deac4d978fed94e59edb1
Rocktavious <rocktavious@icloud.com>
2023-01-26 13:17:53 -0600

    fix typo and beefup example

--- examples/resources/opslevel_trigger_definition/resource.tf
+++ examples/resources/opslevel_trigger_definition/resource.tf
@@ -10,20 +10,66 @@ data "opslevel_filter" "tier_1" {
 }
 
 resource "opslevel_webhook_action" "example" {
-  name = "example"
-  description = "something about what i do"
-  payload = "{\"event_type\": \"{{ input.event }}\"}"
-  url = "https://gitlab.com/api/v4/{{ input.project }}/"
+  name = "Page The On Call"
+  description = "Pages the On Call"
+  url = "https://api.pagerduty.com/incidents"
   method = "POST"
   headers = {
     content-type = "application/json"
-    accept = "application/json"
+    accept = "application/vnd.pagerduty+json;version=2"
+    authorization = "Token token=XXXXXXXXXXXXX"
+    from = "john@opslevel.com"
   }
+  payload = <<EOT
+{
+    "incident":
+    {
+        "type": "incident",
+        "title": "{{manualInputs.IncidentTitle}}",
+        "service": {
+        "id": "{{ service | tag_value: 'pd_id' }}",
+        "type": "service_reference"
+        },
+        "body": {
+        "type": "incident_body",
+        "details": "Incident triggered from OpsLevel by {{user.name}} with the email {{user.email}}. {{manualInputs.IncidentDescription}}"
+        }
+    }
+}
+  EOT
 }
 
 resource "opslevel_trigger_definition" "example" {
-  name = "example"
-  description = "something about what i do"
+  name = "Page The On Call"
+  description = "Pages the On Call"
   owner = data.opslevel_team.platform.id
+  filter = data.opslevel_filter.tier_1.id
   action = opslevel_webhook_action.example.id
+  access_control = "everyone"
+  manual_inputs_definition = <<EOT
+---
+version: 1
+inputs:
+  - identifier: IncidentTitle
+    displayName: Title
+    description: Title of the incident to trigger
+    type: text_input
+    required: true
+    maxLength: 60
+    defaultValue: Service Incident Manual Trigger
+  - identifier: IncidentDescription
+    displayName: Incident Description
+    description: The description of the incident
+    type: text_area
+    required: true
+  EOT
+  response_template = <<EOT
+{% if response.status >= 200 and response.status < 300 %}
+## Congratulations!
+Your request for {{ service.name }} has succeeded. See the incident here: {{response.body.incident.html_url}}
+{% else %}
+## Oops something went wrong
+Talk to the team that owns this action for more details.
+{% endif %}
+  EOT
 }
--- examples/resources/opslevel_webhook_action/resource.tf
+++ examples/resources/opslevel_webhook_action/resource.tf
@@ -1,11 +1,29 @@
 resource "opslevel_webhook_action" "example" {
-  name = "example"
-  description = "something about what i do"
-  payload = "{\"event_type\": \"{{ input.event }}\"}"
-  url = "https://gitlab.com/api/v4/{{ input.project }}/"
+  name = "Page The On Call"
+  description = "Pages the On Call"
+  url = "https://api.pagerduty.com/incidents"
   method = "POST"
   headers = {
     content-type = "application/json"
-    accept = "application/json"
+    accept = "application/vnd.pagerduty+json;version=2"
+    authorization = "Token token=XXXXXXXXXXXXXX"
+    from = "john@opslevel.com"
   }
+  payload = <<EOT
+{
+    "incident":
+    {
+        "type": "incident",
+        "title": "{{manualInputs.IncidentTitle}}",
+        "service": {
+        "id": "{{ service | tag_value: 'pd_id' }}",
+        "type": "service_reference"
+        },
+        "body": {
+        "type": "incident_body",
+        "details": "Incident triggered from OpsLevel by {{user.name}} with the email {{user.email}}. {{manualInputs.IncidentDescription}}"
+        }
+    }
+}
+  EOT
 }
