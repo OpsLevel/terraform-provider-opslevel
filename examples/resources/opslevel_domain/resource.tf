509bf7be231fc1f394fffa1d69c38a3977da8a61
Rocktavious <rocktavious@icloud.com>
2023-03-16 20:31:44 -0500

    add documentation examples

---
+++ examples/resources/opslevel_domain/resource.tf
@@ -0,0 +1,9 @@
+data "opslevel_team" "devs" {
+    alias = "developers"
+}
+
+resource "opslevel_domain" "example" {
+  name = "Example"
+  description = "The whole app in one monolith"
+  owner = data.opslevel_team.devs.alias // or .id
+}
