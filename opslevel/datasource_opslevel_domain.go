c5bf7560735d0f6345d63ed0d7a9c0fa159cb635
Rocktavious <rocktavious@icloud.com>
2023-03-21 20:32:33 -0500

    implement domain datasource

--- opslevel/datasource_opslevel_domain.go
+++ opslevel/datasource_opslevel_domain.go
@@ -7,12 +7,54 @@ import (
 
 func datasourceDomain() *schema.Resource {
 	return &schema.Resource{
-		Read:   wrap(datasourceDomainRead),
-		Schema: map[string]*schema.Schema{},
+		Read: wrap(datasourceDomainRead),
+		Schema: map[string]*schema.Schema{
+			"identifier": {
+				Type:        schema.TypeString,
+				Description: "The id or alias of the domain to find.",
+				ForceNew:    true,
+				Optional:    true,
+			},
+			"aliases": {
+				Type:        schema.TypeList,
+				Description: "The aliases of the domain.",
+				Computed:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The name of the domain.",
+				Computed:    true,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Description: "The description of the domain.",
+				Computed:    true,
+			},
+			"owner": {
+				Type:        schema.TypeString,
+				Description: "The id of the domain owner - could be a group or team.",
+				Computed:    true,
+			},
+		},
 	}
 }
 
 func datasourceDomainRead(d *schema.ResourceData, client *opslevel.Client) error {
+	identifier := d.Get("identifier").(string)
+	resource, err := client.GetDomain(identifier)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(string(resource.Id))
+	d.Set("aliases", resource.Aliases)
+	d.Set("name", resource.Name)
+	d.Set("description", resource.Description)
+	if resource.Owner.GroupId.Id == "" {
+		d.Set("owner", resource.Owner.TeamId.Id)
+	} else {
+		d.Set("owner", resource.Owner.GroupId.Id)
+	}
 
 	return nil
 }
