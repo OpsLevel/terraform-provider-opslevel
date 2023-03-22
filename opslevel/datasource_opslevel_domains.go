d775e006cac808a05990160ae9ac194a5ec06194
Rocktavious <rocktavious@icloud.com>
2023-03-21 20:42:01 -0500

    implement domains datasource

--- opslevel/datasource_opslevel_domains.go
+++ opslevel/datasource_opslevel_domains.go
@@ -7,12 +7,67 @@ import (
 
 func datasourceDomains() *schema.Resource {
 	return &schema.Resource{
-		Read:   wrap(datasourceDomainsRead),
-		Schema: map[string]*schema.Schema{},
+		Read: wrap(datasourceDomainsRead),
+		Schema: map[string]*schema.Schema{
+			"aliases": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type:     schema.TypeList,
+					Computed: true,
+					Elem:     &schema.Schema{Type: schema.TypeString},
+				},
+			},
+			"ids": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"descriptions": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"owners": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		},
 	}
 }
 
 func datasourceDomainsRead(d *schema.ResourceData, client *opslevel.Client) error {
+	resp, err := client.ListDomains(nil)
+	if err != nil {
+		return err
+	}
+
+	count := len(resp.Nodes)
+	aliases := make([][]string, count)
+	ids := make([]string, count)
+	names := make([]string, count)
+	descriptions := make([]string, count)
+	owners := make([]string, count)
+	for i, item := range resp.Nodes {
+		aliases[i] = item.Aliases
+		ids[i] = string(item.Id)
+		names[i] = item.Name
+		descriptions[i] = item.Description
+		owners[i] = item.Owner.Id()
+	}
+
+	d.SetId(timeID())
+	d.Set("aliases", aliases)
+	d.Set("ids", ids)
+	d.Set("names", names)
+	d.Set("descriptions", descriptions)
+	d.Set("owners", owners)
 
 	return nil
 }
