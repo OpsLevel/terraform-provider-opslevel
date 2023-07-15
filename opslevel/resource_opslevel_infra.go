84c9ed58ae8d1f76b9c14625210fdd4714d8a7f9
Rocktavious <rocktavious@icloud.com>
2023-07-15 13:19:01 -0500

    update opslevel-go and improve schema of infra resource

--- opslevel/resource_opslevel_infra.go
+++ opslevel/resource_opslevel_infra.go
@@ -18,46 +18,53 @@ func resourceInfrastructure() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
-			"type": {
-				Type:        schema.TypeString,
-				Description: "The type of the infrastructure resource in its provider.",
-				Optional:    true,
-				ForceNew:    false,
+			"aliases": {
+				Type:        schema.TypeList,
+				Description: "The aliases of the infrastructure resource.",
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 			"schema": {
 				Type:        schema.TypeString,
-				Description: "The schema of the infrastructure resource that determines its specification.",
+				Description: "The schema of the infrastructure resource that determines its data specification.",
 				Required:    true,
 				ForceNew:    true,
 			},
 			"owner": {
 				Type:        schema.TypeString,
-				Description: "The id of the owner for the infrastructure resource.  Can be a team or group",
+				Description: "The id of the owner for the infrastructure resource.  Can be a team or group. Does not support aliases!",
 				ForceNew:    false,
 				Optional:    true,
 			},
 			"provider_data": {
 				Type:        schema.TypeList,
-				Description: "The provider data for the infrastructure resource",
+				Description: "The provider specific data for the infrastructure resource.",
 				ForceNew:    false,
 				Optional:    true,
+				MaxItems:    1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"account_name": {
+						"name": {
 							Type:        schema.TypeString,
-							Description: "The account name for the infrastructure resource.",
+							Description: "The name of the provider of the infrastructure resource. (eg. AWS, GCP, Azure)",
 							ForceNew:    false,
 							Optional:    true,
 						},
-						"external_url": {
+						"type": {
 							Type:        schema.TypeString,
-							Description: "The external url for the infrastructure resource.",
+							Description: "The type of the infrastructure resource as defined by its provider.",
 							ForceNew:    false,
 							Optional:    true,
 						},
-						"provider_name": {
+						"account": {
 							Type:        schema.TypeString,
-							Description: "The provider name for the infrastructure resource.",
+							Description: "The canonical account name for the provider of the infrastructure resource.",
+							ForceNew:    false,
+							Required:    true,
+						},
+						"url": {
+							Type:        schema.TypeString,
+							Description: "The url for the provider of the infrastructure resource.",
 							ForceNew:    false,
 							Optional:    true,
 						},
@@ -69,37 +76,39 @@ func resourceInfrastructure() *schema.Resource {
 				Description: "The data of the infrastructure resource in JSON format.",
 				Optional:    true,
 			},
-			"aliases": {
-				Type:        schema.TypeList,
-				Description: "The aliases of the infrastructure resource.",
-				Computed:    true,
-				Elem:        &schema.Schema{Type: schema.TypeString},
-			},
 		},
 	}
 }
 
-func expandInfrastructureProviderData(d *schema.ResourceData) *opslevel.InfrastructureResourceProviderInput {
-	output := opslevel.InfrastructureResourceProviderInput{}
+func flattenInfraProviderData(resource *opslevel.InfrastructureResource) []map[string]any {
+	return []map[string]any{{
+		"account": resource.ProviderData.AccountName,
+		"name":    resource.ProviderData.ProviderName,
+		"type":    resource.ProviderType,
+		"url":     resource.ProviderData.ExternalURL,
+	}}
+}
+
+func expandInfraProviderData(d *schema.ResourceData) *opslevel.InfraProviderInput {
 	config := d.Get("provider_data").([]interface{})
 	if len(config) > 0 {
 		item := config[0].(map[string]interface{})
-		output.AccountName = item["account_name"].(string)
-		output.ExternalURL = item["external_url"].(string)
-		output.ProviderName = item["provider_name"].(string)
+		return &opslevel.InfraProviderInput{
+			Account: item["account"].(string),
+			Name:    item["name"].(string),
+			Type:    item["type"].(string),
+			URL:     item["url"].(string),
+		}
 	}
-	return &output
+	return &opslevel.InfraProviderInput{}
 }
 
 func resourceInfrastructureCreate(d *schema.ResourceData, client *opslevel.Client) error {
-	resource, err := client.CreateInfrastructure(opslevel.InfrastructureResourceInput{
-		Type: GetString(d, "type"),
-		Schema: &opslevel.InfrastructureResourceSchemaInput{
-			Type: *GetString(d, "schema"),
-		},
-		ProviderData: expandInfrastructureProviderData(d),
-		Owner:        opslevel.NewID(d.Get("owner").(string)),
-		Data:         opslevel.NewJSON(*GetString(d, "data")),
+	resource, err := client.CreateInfrastructure(opslevel.InfraInput{
+		Schema:   d.Get("schema").(string),
+		Owner:    opslevel.NewID(d.Get("owner").(string)),
+		Provider: expandInfraProviderData(d),
+		Data:     opslevel.NewJSON(d.Get("data").(string)),
 	})
 	if err != nil {
 		return err
@@ -116,22 +125,19 @@ func resourceInfrastructureRead(d *schema.ResourceData, client *opslevel.Client)
 		return err
 	}
 
+	if err := d.Set("schema", resource.Schema); err != nil {
+		return err
+	}
 	if err := d.Set("aliases", resource.Aliases); err != nil {
 		return err
 	}
-	if err := d.Set("type", resource.Type); err != nil {
-		return err
-	}
-	// TODO: check if this is not needed - but i fear Terraform might need this and our API doesn expose it.
-	//if err := d.Set("schema", resource.Schema.Type); err != nil {
-	//	return err
-	//}
 	if err := d.Set("owner", resource.Owner.Id()); err != nil {
 		return err
 	}
-
-	// TODO: this might not work properly - might need to use resource.Data.MarshalJSON()
-	if err := d.Set("data", resource.Data); err != nil {
+	if err := d.Set("provider_data", flattenInfraProviderData(resource)); err != nil {
+		return err
+	}
+	if err := d.Set("data", resource.Data.ToJSON()); err != nil {
 		return err
 	}
 
@@ -141,26 +147,18 @@ func resourceInfrastructureRead(d *schema.ResourceData, client *opslevel.Client)
 func resourceInfrastructureUpdate(d *schema.ResourceData, client *opslevel.Client) error {
 	id := d.Id()
 
-	// TODO: i'm using a different pattern here then in other update functions, but i think this is actually better
-	// please make sure to test the update function heavily so we don't break anything
-	// make sure that each individual field change changed and doesn't cause an infinite plan change
-	input := opslevel.InfrastructureResourceInput{
-		Type: GetString(d, "type"),
-		Schema: &opslevel.InfrastructureResourceSchemaInput{
-			Type: *GetString(d, "schema"),
-		},
-		ProviderData: expandInfrastructureProviderData(d),
-		Owner:        opslevel.NewID(d.Get("owner").(string)),
-		Data:         opslevel.NewJSON(*GetString(d, "data")),
-	}
-
-	_, err := client.UpdateInfrastructure(id, input)
+	_, err := client.UpdateInfrastructure(id, opslevel.InfraInput{
+		Schema:   d.Get("schema").(string),
+		Owner:    opslevel.NewID(d.Get("owner").(string)),
+		Provider: expandInfraProviderData(d),
+		Data:     opslevel.NewJSON(d.Get("data").(string)),
+	})
 	if err != nil {
 		return err
 	}
 
 	d.Set("last_updated", timeLastUpdated())
-	return resourceSystemRead(d, client)
+	return resourceInfrastructureRead(d, client)
 }
 
 func resourceInfrastructureDelete(d *schema.ResourceData, client *opslevel.Client) error {
