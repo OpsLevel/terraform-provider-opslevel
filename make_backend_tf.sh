41d8240f9d1383d5d951e36cd0a9130461ea4d44
David Bloss <david@opslevel.com>
2023-08-17 11:49:32 -0500

    add make backend script, updated Taskfile

---
+++ make_backend_tf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+cat << EOF > backend.tf
+terraform {
+  required_providers {
+    opslevel = {
+      source  = "OpsLevel/opslevel"
+      version = "> ${OPSLEVEL_TERRAFORM_SOURCE_VERSION:-0.0.1}"
+    }
+  }
+}
+
+provider "opslevel" {}
+EOF
