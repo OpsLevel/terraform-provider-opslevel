a487c385bf9dc5b5392b33185f0b844f2d3aa1f6
David Bloss <david@opslevel.com>
2023-09-21 14:10:19 -0500

    update opslevel tag resource

--- examples/resources/opslevel_tag/import.sh
+++
@@ -1,1 +0,0 @@
-terraform import opslevel_tag.foo Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS82MDI0
--- opslevel/resource_opslevel_tag.go
+++ opslevel/resource_opslevel_tag.go
@@ -2,33 +2,24 @@ package opslevel
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/opslevel/opslevel-go/v2023"
 )
 
 func resourceTag() *schema.Resource {
 	return &schema.Resource{
-		Description: "Manages a tag.",
+		Description: "Manages a tag. Only uses 'tag create' API due to reasons",
 		Create:      wrap(resourceTagCreate),
 		Read:        wrap(resourceTagRead),
 		Update:      wrap(resourceTagUpdate),
 		Delete:      wrap(resourceTagDelete),
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
 		Schema: map[string]*schema.Schema{
 			"last_updated": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
-			},
-			"identifier": {
-				Type:        schema.TypeString,
-				Description: "The id or human-friendly, unique identifier for the tag.",
-				ForceNew:    true,
-				Optional:    true,
 			},
 			"resource_id": {
 				Type:        schema.TypeString,
@@ -36,11 +27,12 @@ func resourceTag() *schema.Resource {
 				ForceNew:    true,
 				Optional:    true,
 			},
-			"type": {
-				Type:        schema.TypeString,
-				Description: "The resource type that the tag applies to.",
-				ForceNew:    false,
-				Optional:    true,
+			"resource_type": {
+				Type:         schema.TypeString,
+				Description:  "The resource type that the tag applies to.",
+				ForceNew:     true,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(opslevel.AllTaggableResource, false),
 			},
 			"key": {
 				Type:        schema.TypeString,
@@ -54,59 +46,73 @@ func resourceTag() *schema.Resource {
 				ForceNew:    false,
 				Optional:    true,
 			},
+			"returned_resource": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
 func resourceTagCreate(d *schema.ResourceData, client *opslevel.Client) error {
-	resourceId := d.Get("resource_id").(string)
-	resourceType, err := getValidatedTaggableResource(d.Get("type").(string))
-	if err != nil {
-		return err
-	}
+	resourceType := opslevel.TaggableResource(d.Get("resource_type").(string))
 
 	tagCreateInput := opslevel.TagCreateInput{
 		Key:   d.Get("key").(string),
 		Value: d.Get("value").(string),
 		Type:  resourceType,
 	}
+
+	resourceId := d.Get("resource_id").(string)
 	if opslevel.IsID(resourceId) {
 		tagCreateInput.Id = opslevel.ID(resourceId)
 	} else {
 		tagCreateInput.Alias = resourceId
 	}
-
-	newTag, _ := client.CreateTag(tagCreateInput)
+	newTag, err := client.CreateTag(tagCreateInput)
 	if err != nil {
 		return err
 	}
 
-	d.Set("type", resourceType)
+	resource, err := getResource(resourceType, resourceId, client)
+	if err != nil {
+		return err
+	}
+
 	d.SetId(string(newTag.Id))
+	d.Set("returned_resource", resource.ResourceId())
+	d.Set("resource_type", resourceType)
 
 	return resourceTagRead(d, client)
 }
 
 func resourceTagRead(d *schema.ResourceData, client *opslevel.Client) error {
-	id := d.Id()
-	resource, err := getResource(d.Get("type").(string), id, client)
+	resourceId := d.Get("returned_resource").(string)
+	resourceType := opslevel.TaggableResource(d.Get("resource_type").(string))
+	resource, err := getResource(resourceType, resourceId, client)
 	if err != nil {
 		return err
 	}
 
-	if err := d.Set("identifier", id); err != nil {
-		return err
-	}
-
-	if err := d.Set("resource_id", resource.ResourceId()); err != nil {
-		return err
-	}
-
-	if err := d.Set("type", resource.ResourceType()); err != nil {
-		return err
-	}
-
+	id := d.Id()
 	tag := resource.GetTag(*opslevel.NewID(id), client)
+	if tag == nil {
+		return fmt.Errorf(
+			"Tag '%s' for resource with id: '%s' of type '%s' not found",
+			id,
+			resource.ResourceId(),
+			resource.ResourceType(),
+		)
+	}
+
+	if err := d.Set("resource_id", d.Get("resource_id").(string)); err != nil {
+		return err
+	}
+
+	if err := d.Set("resource_type", string(resource.ResourceType())); err != nil {
+		return err
+	}
+
 	if err := d.Set("key", tag.Key); err != nil {
 		return err
 	}
@@ -121,77 +127,68 @@ func resourceTagRead(d *schema.ResourceData, client *opslevel.Client) error {
 func resourceTagUpdate(d *schema.ResourceData, client *opslevel.Client) error {
 	id := d.Id()
 
-	input := opslevel.TagUpdateInput{
-		Id: opslevel.ID(id),
-	}
-
-	if d.HasChange("key") {
-		input.Key = d.Get("key").(string)
-	}
-
-	if d.HasChange("value") {
-		input.Value = d.Get("value").(string)
-	}
-
-	_, err := client.UpdateTag(input)
+	resourceId := d.Get("resource_id").(string)
+	resourceType := opslevel.TaggableResource(d.Get("resource_type").(string))
+	resource, err := getResource(resourceType, resourceId, client)
 	if err != nil {
 		return err
 	}
 
+	input := opslevel.TagUpdateInput{
+		Id:    opslevel.ID(id),
+		Key:   d.Get("key").(string),
+		Value: d.Get("value").(string),
+	}
+
+	if _, err := client.UpdateTag(input); err != nil {
+		return err
+	}
+
+	d.Set("returned_resource", resource.ResourceId())
 	d.Set("last_updated", timeLastUpdated())
 	return resourceTagRead(d, client)
 }
 
 func resourceTagDelete(d *schema.ResourceData, client *opslevel.Client) error {
 	id := d.Id()
-	err := client.DeleteTag(opslevel.ID(id))
-	if err != nil {
+	if err := client.DeleteTag(opslevel.ID(id)); err != nil {
 		return err
 	}
 	d.SetId("")
 	return nil
 }
 
-func getValidatedTaggableResource(providedResourceType string) (opslevel.TaggableResource, error) {
-	lowerCaseResourceType := strings.ToLower(providedResourceType)
-	for _, taggableResource := range opslevel.AllTaggableResource {
-		if lowerCaseResourceType == strings.ToLower(taggableResource) {
-			return opslevel.TaggableResource(taggableResource), nil
-		}
-	}
-	return opslevel.TaggableResource(""), fmt.Errorf("Unknown resource type '%s'", providedResourceType)
-}
-
-func getResource(validResourceType string, identifier string, client *opslevel.Client) (opslevel.TaggableResourceInterface, error) {
+func getResource(validResourceType opslevel.TaggableResource, identifier string, client *opslevel.Client) (opslevel.TaggableResourceInterface, error) {
+	var err error
 	var taggableResource opslevel.TaggableResourceInterface
 
 	switch validResourceType {
-	case string(opslevel.TaggableResourceService):
+	case opslevel.TaggableResourceService:
 		if opslevel.IsID(identifier) {
-			client.GetService(opslevel.ID(identifier))
+			taggableResource, err = client.GetService(opslevel.ID(identifier))
 		} else {
-			client.GetServiceWithAlias(identifier)
+			taggableResource, err = client.GetServiceWithAlias(identifier)
 		}
-	case string(opslevel.TaggableResourceRepository):
+	case opslevel.TaggableResourceRepository:
 		if opslevel.IsID(identifier) {
-			client.GetRepository(opslevel.ID(identifier))
+			taggableResource, err = client.GetRepository(opslevel.ID(identifier))
 		} else {
-			client.GetRepositoryWithAlias(identifier)
+			taggableResource, err = client.GetRepositoryWithAlias(identifier)
 		}
-	case string(opslevel.TaggableResourceTeam):
+	case opslevel.TaggableResourceTeam:
 		if opslevel.IsID(identifier) {
-			client.GetTeam(opslevel.ID(identifier))
+			taggableResource, err = client.GetTeam(opslevel.ID(identifier))
 		} else {
-			client.GetTeamWithAlias(identifier)
+			taggableResource, err = client.GetTeamWithAlias(identifier)
 		}
-	case string(opslevel.TaggableResourceUser):
-		client.GetUser(identifier)
-	case string(opslevel.TaggableResourceDomain):
-		client.GetDomain(identifier)
-	case string(opslevel.TaggableResourceSystem):
-		client.GetSystem(identifier)
-	case string(opslevel.TaggableResourceInfrastructureresource):
-		client.GetInfrastructure(identifier)
+	case opslevel.TaggableResourceDomain:
+		taggableResource, err = client.GetDomain(identifier)
+	case opslevel.TaggableResourceSystem:
+		taggableResource, err = client.GetSystem(identifier)
+	}
+
+	if err != nil {
+		return nil, err
 	}
 	return taggableResource, nil
 }
