bd2975d481e6fc5871743fef3296b242a293c2ed
Taimoor Ahmad <taimoor@opslevel.com>
2023-12-22 10:25:33 -0500

    add example with an object as value

--- examples/resources/opslevel_property_assignment/resource.tf
+++ examples/resources/opslevel_property_assignment/resource.tf
@@ -9,3 +9,9 @@ resource "opslevel_property_assignment" "example_2" {
   owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
   value = "true"
 }
+
+resource "opslevel_property_assignment" "example_3" {
+  definition = "Z2lkOi8vb3BzbGV2ZWwvUHJvcGVydGllczo6RGVmaW5pdGlvbi8xODI"
+  owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
+  value = jsonencode({"container_id":"1c6098d6-952a-4062-9293-1dc06e991118", "container_name":"gcr.io/containername"})
+}
