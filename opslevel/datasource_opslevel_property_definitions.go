a86a090c6383e02fffaba32dee26fe30da47049b
taimoor ahmad <139589712+taimoor-at-opslevel@users.noreply.github.com>
2023-12-22 13:21:19 -0500

    Patch Property Definition in terraform for new changes (#175)

--- opslevel/datasource_opslevel_property_definitions.go
+++ opslevel/datasource_opslevel_property_definitions.go
@@ -20,7 +20,17 @@ func datasourcePropertyDefinitions() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
+			"descriptions": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
 			"schemas": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"property_display_statuses": {
 				Type:     schema.TypeList,
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
@@ -38,17 +48,23 @@ func datasourcePropertyDefinitionsRead(d *schema.ResourceData, client *opslevel.
 	count := len(resources.Nodes)
 	ids := make([]string, count)
 	names := make([]string, count)
+	descriptions := make([]string, count)
 	schemas := make([]string, count)
+	propertyDisplayStatuses := make([]string, count)
 	for i, item := range resources.Nodes {
 		ids[i] = string(item.Id)
 		names[i] = item.Name
+		descriptions[i] = item.Description
 		schemas[i] = item.Schema.ToJSON()
+		propertyDisplayStatuses[i] = string(item.PropertyDisplayStatus)
 	}
 
 	d.SetId(timeID())
 	d.Set("ids", ids)
 	d.Set("names", names)
+	d.Set("descriptions", descriptions)
 	d.Set("schemas", schemas)
+	d.Set("property_display_statuses", propertyDisplayStatuses)
 
 	return nil
 }
