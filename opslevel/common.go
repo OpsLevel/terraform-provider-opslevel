a5704d42bb07554a84b7405e430bf1aa1e9c32bd
taimoor ahmad <139589712+taimoor-at-opslevel@users.noreply.github.com>
2023-12-22 14:48:54 -0500

    Assign Properties on a Service in terraform (#174)

--- opslevel/common.go
+++ opslevel/common.go
@@ -306,6 +306,22 @@ func mapMembershipsArray(members *opslevel.TeamMembershipConnection) []map[strin
 	return output
 }
 
+func mapServiceProperties(properties *opslevel.ServicePropertiesConnection) []map[string]any {
+	output := []map[string]any{}
+	for _, property := range properties.Nodes {
+		asMap := make(map[string]any)
+		asMap["definition"] = string(property.Definition.Id)
+		asMap["owner"] = string(property.Owner.Id())
+		if property.Value == nil {
+			asMap["value"] = "null"
+		} else {
+			asMap["value"] = string(*property.Value)
+		}
+		output = append(output, asMap)
+	}
+	return output
+}
+
 func flattenTeamsArray(teams *opslevel.TeamConnection) []string {
 	output := []string{}
 	for _, team := range teams.Nodes {
