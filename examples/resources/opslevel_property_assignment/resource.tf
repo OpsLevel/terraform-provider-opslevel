ae7e6b4a9f15b06637b8d04d13d04dc27c931fcb
Taimoor Ahmad <taimoor@opslevel.com>
2023-12-22 12:11:16 -0500

    use jsonencode(true) in example
    Co-authored-by: David Bloss <david@opslevel.com>

--- examples/resources/opslevel_property_assignment/resource.tf
+++ examples/resources/opslevel_property_assignment/resource.tf
@@ -7,7 +7,7 @@ resource "opslevel_property_assignment" "example" {
 resource "opslevel_property_assignment" "example_2" {
   definition = "Z2lkOi8vb3BzbGV2ZWwvUHJvcGVydGllczo6RGVmaW5pdGlvbi85OA"
   owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
-  value = "true"
+  value = jsonencode(true)
 }
 
 resource "opslevel_property_assignment" "example_3" {
