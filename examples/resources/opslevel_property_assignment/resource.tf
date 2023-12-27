9b4aedc62f451fd7e9236a614940b5072ef363f9
David Bloss <david@opslevel.com>
2023-12-27 15:58:31 -0600

    apply fixes from "terraform fmt -recursive -write=true"

--- examples/resources/opslevel_property_assignment/resource.tf
+++ examples/resources/opslevel_property_assignment/resource.tf
@@ -1,17 +1,17 @@
 resource "opslevel_property_assignment" "example" {
   definition = "Z2lkOi8vb3BzbGV2ZWwvUHJvcGVydGllczo6RGVmaW5pdGlvbi8xODA"
-  owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
-  value = jsonencode("green")
+  owner      = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
+  value      = jsonencode("green")
 }
 
 resource "opslevel_property_assignment" "example_2" {
   definition = "Z2lkOi8vb3BzbGV2ZWwvUHJvcGVydGllczo6RGVmaW5pdGlvbi85OA"
-  owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
-  value = jsonencode(true)
+  owner      = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
+  value      = jsonencode(true)
 }
 
 resource "opslevel_property_assignment" "example_3" {
   definition = "Z2lkOi8vb3BzbGV2ZWwvUHJvcGVydGllczo6RGVmaW5pdGlvbi8xODI"
-  owner = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
-  value = jsonencode({"container_id":"1c6098d6-952a-4062-9293-1dc06e991118", "container_name":"gcr.io/containername"})
+  owner      = "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS85MTQyOQ"
+  value      = jsonencode({ "container_id" : "1c6098d6-952a-4062-9293-1dc06e991118", "container_name" : "gcr.io/containername" })
 }
--- examples/resources/opslevel_property_definition/resource.tf
+++ examples/resources/opslevel_property_definition/resource.tf
@@ -1,4 +1,4 @@
 resource "opslevel_property_definition" "newpd" {
-  name        = "Friends Property"
-  schema     =  jsonencode({"$ref":"#/$defs/MyProp","$defs":{"MyProp":{"properties":{"name":{"type":"string","title":"the new name","description":"The name of a friend","default":"alex","examples":["joe","lucy"]}},"additionalProperties":false,"type":"object","required":["name"]}}})
+  name   = "Friends Property"
+  schema = jsonencode({ "$ref" : "#/$defs/MyProp", "$defs" : { "MyProp" : { "properties" : { "name" : { "type" : "string", "title" : "the new name", "description" : "The name of a friend", "default" : "alex", "examples" : ["joe", "lucy"] } }, "additionalProperties" : false, "type" : "object", "required" : ["name"] } } })
 }
