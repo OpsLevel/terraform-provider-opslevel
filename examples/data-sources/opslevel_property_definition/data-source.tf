76dfb69e7bfb4548d6b67b0af1d89be00bfa0ddc
David Bloss <david@opslevel.com>
2024-01-12 16:01:44 -0600

    format fixes in example tf files

--- examples/data-sources/opslevel_property_definition/data-source.tf
+++ examples/data-sources/opslevel_property_definition/data-source.tf
@@ -1,7 +1,7 @@
 data "opslevel_property_definition" "pd1" {
-    identifier = "id_or_alias"
+  identifier = "id_or_alias"
 }
 
 output "pd_schema" {
-    value = data.opslevel_property_definition.pd1.schema
+  value = data.opslevel_property_definition.pd1.schema
 }
