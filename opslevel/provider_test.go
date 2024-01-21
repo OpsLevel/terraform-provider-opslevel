bbd150033bab43a3d5132cd5b188ef96321bb70b
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-21 13:28:53 -0500

    can remove more stuff from provider_test.go

--- opslevel/provider_test.go
+++ opslevel/provider_test.go
@@ -4,23 +4,10 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
-
-var (
-	testAccProvider *schema.Provider
-)
-
-func init() {
-	testAccProvider = Provider().(*schema.Provider)
-}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
-}
-
-func TestProviderImpl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
 }
