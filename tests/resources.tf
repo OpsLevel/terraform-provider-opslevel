f5ba2b8077cd96e0b1b62f3b11b17dfa5d314ed7
David Bloss <david@opslevel.com>
2024-03-22 13:41:19 -0700

    add user resource, with tests (#232)
    * add user resource, needs tests
    
    * add tests for user resource
    
    * terraform format fix

--- tests/resources.tf
+++ tests/resources.tf
@@ -5,3 +5,19 @@ resource "opslevel_domain" "fancy" {
   note        = "This is an example"
 }
 
+resource "opslevel_user" "mock_user" {
+  name  = "Mock User"
+  email = "mock_user@mock.com"
+  role  = "user"
+}
+
+resource "opslevel_user" "mock_user_no_role" {
+  name  = "Mock User"
+  email = "mock_user@mock.com"
+}
+
+resource "opslevel_user" "mock_user_admin" {
+  name  = "Mock User"
+  email = "mock_user@mock.com"
+  role  = "admin"
+}
