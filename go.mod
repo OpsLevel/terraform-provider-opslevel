9fa5431fe4e864ba4a97d748f608fecc76505c53
Rocktavious <rocktavious@icloud.com>
2024-04-05 08:56:16 -0500

    First Pass at Manual Check

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/hashicorp/terraform-plugin-go v0.22.1
 	github.com/hashicorp/terraform-plugin-log v0.9.0
 	github.com/opslevel/opslevel-go/v2024 v2024.3.15
+	golang.org/x/net v0.22.0
 )
 
 require (
@@ -27,6 +28,7 @@ require (
 	github.com/hashicorp/go-plugin v1.6.0 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/terraform-plugin-framework-timetypes v0.3.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.2.3 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
@@ -49,7 +51,6 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
--- opslevel/resource_opslevel_check_manual.go
+++ opslevel/resource_opslevel_check_manual.go
@@ -1,5 +1,100 @@
 package opslevel
 
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/opslevel/opslevel-go/v2024"
+)
+
+var _ resource.ResourceWithConfigure = &CheckManualResource{}
+var _ resource.ResourceWithImportState = &CheckManualResource{}
+
+func NewCheckManualResource() resource.Resource {
+	return &CheckManualResource{}
+}
+
+// CheckManualResource defines the resource implementation.
+type CheckManualResource struct {
+	CommonResourceClient
+}
+
+type CheckUpdateFrequency struct {
+	StartingDate timetypes.RFC3339 `tfsdk:"starting_date"`
+	TimeScale    types.String      `tfsdk:"time_scale"`
+	Value        types.Int64       `tfsdk:"value"`
+}
+
+type CheckManualResourceModel struct {
+	CheckBaseModel
+	UpdateFrequency       CheckUpdateFrequency `tfsdk:"update_frequency"`
+	UpdateRequiresComment types.Bool           `tfsdk:"update_requires_comment"`
+}
+
+func (r *CheckManualResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_check_manual"
+}
+
+func (r *CheckManualResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Check Manual Resource",
+
+		Attributes: CheckBaseAttributes(map[string]schema.Attribute{
+			"update_requires_comment": schema.BoolAttribute{
+				Description: "Whether the check requires a comment or not.",
+				Required:    true,
+			},
+		}),
+		Blocks: map[string]schema.Block{
+			"update_frequency": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"starting_date": schema.StringAttribute{
+						Description: "The date that the check will start to evaluate.",
+						Required:    true,
+					},
+					"time_scale": schema.StringAttribute{
+						Description: "The time scale type for the frequency.",
+						Required:    true,
+						Validators: []validator.String{
+							stringvalidator.OneOf(opslevel.AllFrequencyTimeScale...),
+						},
+					},
+					"value": schema.Int64Attribute{
+						Description: "The value to be used together with the frequency time_scale.",
+						Required:    true,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *CheckManualResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+}
+
+func (r *CheckManualResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+}
+
+func (r *CheckManualResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+
+}
+
+func (r *CheckManualResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+
+}
+
+func (r *CheckManualResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
 // import (
 // 	"fmt"
 // 	"time"
