e445012a48ee04c8367fddab113de375e4793863
Rocktavious <rocktavious@icloud.com>
2024-04-05 15:23:08 -0500

    finish out the CRUD

--- opslevel/resource_opslevel_check_base.go
+++ opslevel/resource_opslevel_check_base.go
@@ -1,20 +1,27 @@
 package opslevel
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/opslevel/opslevel-go/v2024"
+	"github.com/relvacode/iso8601"
+	"time"
 )
 
 type CheckBaseModel struct {
-	Category    types.String `tfsdk:"category"`
-	Description types.String `tfsdk:"description"`
-	EnableOn    types.String `tfsdk:"enable_on"`
-	Enabled     types.Bool   `tfsdk:"enabled"`
-	Filter      types.String `tfsdk:"filter"`
-	Level       types.String `tfsdk:"level"`
-	Name        types.String `tfsdk:"name"`
-	Notes       types.String `tfsdk:"notes"`
-	Owner       types.String `tfsdk:"owner"`
+	Category    types.String      `tfsdk:"category"`
+	Description types.String      `tfsdk:"description"`
+	Enabled     types.Bool        `tfsdk:"enabled"`
+	EnableOn    timetypes.RFC3339 `tfsdk:"enable_on"`
+	Filter      types.String      `tfsdk:"filter"`
+	Id          types.String      `tfsdk:"id"`
+	Level       types.String      `tfsdk:"level"`
+	Name        types.String      `tfsdk:"name"`
+	Notes       types.String      `tfsdk:"notes"`
+	Owner       types.String      `tfsdk:"owner"`
+	LastUpdated timetypes.RFC3339 `tfsdk:"last_updated"`
 }
 
 func CheckBaseAttributes(attrs map[string]schema.Attribute) map[string]schema.Attribute {
@@ -27,16 +34,16 @@ func CheckBaseAttributes(attrs map[string]schema.Attribute) map[string]schema.At
 			Description: "The description the check.",
 			Optional:    true,
 		},
+		"enabled": schema.BoolAttribute{
+			Description: "Whether the check is enabled or not.  Do not use this field in tandem with 'enable_on'.",
+			Optional:    true,
+		},
 		"enable_on": schema.StringAttribute{
 			Description: `The date when the check will be automatically enabled.
  If you use this field you should add both 'enabled' and 'enable_on' to the lifecycle ignore_changes settings.
  See example in opslevel_check_manual for proper configuration.
  `,
 			Optional: true,
-		},
-		"enabled": schema.BoolAttribute{
-			Description: "Whether the check is enabled or not.  Do not use this field in tandem with 'enable_on'.",
-			Optional:    true,
 		},
 		"filter": schema.StringAttribute{
 			Description: "The id of the filter of the check.",
@@ -58,9 +65,67 @@ func CheckBaseAttributes(attrs map[string]schema.Attribute) map[string]schema.At
 			Description: "The id of the team that owns the check.",
 			Optional:    true,
 		},
+		"last_updated": schema.StringAttribute{
+			Optional: true,
+			Computed: true,
+		},
 	}
 	for key, value := range attrs {
 		output[key] = value
 	}
 	return output
+}
+
+func AsID(input types.String) opslevel.ID {
+	return *opslevel.NewID(input.ValueString())
+}
+
+func AsISO8601(input timetypes.RFC3339) (*iso8601.Time, diag.Diagnostics) {
+	// TODO: covering up a diagnostics error :(
+	t, diags := input.ValueRFC3339Time()
+	return &iso8601.Time{Time: t}, diags
+}
+
+func NewCheckCreateInputFrom[T any](model CheckBaseModel) (*T, diag.Diagnostics) {
+	enabledOn, diags := AsISO8601(model.EnableOn)
+	input := opslevel.CheckCreateInput{
+		Category: *opslevel.NewID(model.Category.ValueString()),
+		Enabled:  model.Enabled.ValueBoolPointer(),
+		EnableOn: enabledOn,
+		Filter:   opslevel.NewID(model.Filter.ValueString()),
+		Level:    *opslevel.NewID(model.Level.ValueString()),
+		Name:     model.Name.ValueString(),
+		Notes:    model.Notes.ValueStringPointer(),
+		Owner:    opslevel.NewID(model.Owner.ValueString()),
+	}
+	return opslevel.NewCheckCreateInputTypeOf[T](input), diags
+}
+
+func NewCheckUpdateInputFrom[T any](model CheckBaseModel) (*T, diag.Diagnostics) {
+	enabledOn, diags := AsISO8601(model.EnableOn)
+	input := opslevel.CheckUpdateInput{
+		Category: *opslevel.NewID(model.Category.ValueString()),
+		Enabled:  model.Enabled.ValueBoolPointer(),
+		EnableOn: enabledOn,
+		Filter:   opslevel.NewID(model.Filter.ValueString()),
+		Level:    *opslevel.NewID(model.Level.ValueString()),
+		Id:       *opslevel.NewID(model.Id.ValueString()),
+		Name:     model.Name.ValueString(),
+		Notes:    model.Notes.ValueStringPointer(),
+		Owner:    opslevel.NewID(model.Owner.ValueString()),
+	}
+	return opslevel.NewCheckUpdateInputTypeOf[T](input), diags
+}
+
+func ApplyCheckBaseModel(check opslevel.Check, model *CheckBaseModel) {
+	model.Category = types.StringValue(string(check.Category.Id))
+	model.Enabled = types.BoolValue(check.Enabled)
+	model.EnableOn = timetypes.NewRFC3339TimeValue(check.EnableOn.Time)
+	model.Filter = types.StringValue(string(check.Filter.Id))
+	model.Id = types.StringValue(string(check.Id))
+	model.Level = types.StringValue(string(check.Level.Id))
+	model.Name = types.StringValue(check.Name)
+	model.Notes = types.StringValue(check.Notes)
+	model.Owner = types.StringValue(string(check.Owner.Team.Id))
+	model.LastUpdated = timetypes.NewRFC3339TimeValue(time.Now())
 }
