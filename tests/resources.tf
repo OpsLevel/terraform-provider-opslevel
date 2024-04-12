13537c899ab708de8cdaa48aabd7c1de3d6e6bc8
Rocktavious <rocktavious@icloud.com>
2024-04-11 21:29:04 -0500

    Add service property check

--- tests/resources.tf
+++ tests/resources.tf
@@ -173,3 +173,19 @@ resource "opslevel_check_manual" "example" {
   update_requires_comment = false
   notes                   = "Optional additional info on why this check is run or how to fix it"
 }
+
+# Check Service Property
+
+resource "opslevel_check_service_property" "example" {
+  name     = "foo"
+  enabled  = true
+  category = var.test_id
+  level    = var.test_id
+  owner    = var.test_id
+  filter   = var.test_id
+  property = "language"
+  predicate = {
+    type  = "equals"
+    value = "python"
+  }
+}
