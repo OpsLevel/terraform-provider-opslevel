6bbd644e24615b84467c98ad7a598d7b4c048174
Kyle <kyle@opslevel.com>
2024-07-29 14:43:49 -0500

    Update opslevel/resource_opslevel_check_package_version.go
    Co-authored-by: David Bloss <david@opslevel.com>

--- opslevel/resource_opslevel_check_package_version.go
+++ opslevel/resource_opslevel_check_package_version.go
@@ -101,7 +101,6 @@ func (r *CheckPackageVersionResource) Metadata(ctx context.Context, req resource
 
 func (r *CheckPackageVersionResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		Version: 1,
 		// This description is used by the documentation generator and the language server.
 		MarkdownDescription: "Check PackageVersion Resource",
 
