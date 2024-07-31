45751621b7819f857f583b9b52359f66bd2398e2
Rocktavious <rocktavious@icloud.com>
2024-07-31 14:52:23 -0500

    fix example lint

--- examples/resources/opslevel_check_package_version/resource.tf
+++ examples/resources/opslevel_check_package_version/resource.tf
@@ -24,48 +24,48 @@ data "opslevel_filter" "tier1" {
 }
 
 resource "opslevel_check_package_version" "example" {
-  name      = "foo"
-  enable = true
-  category  = data.opslevel_rubric_category.security.id
-  level     = data.opslevel_rubric_level.bronze.id
-  notes     = "Optional additional info on why this check is run or how to fix it"
+  name     = "foo"
+  enable   = true
+  category = data.opslevel_rubric_category.security.id
+  level    = data.opslevel_rubric_level.bronze.id
+  notes    = "Optional additional info on why this check is run or how to fix it"
 
   package_constraint = "exists"
-  package_manager = "gradle"
-  package_name = "log4j"
+  package_manager    = "gradle"
+  package_name       = "log4j"
 }
 
 resource "opslevel_check_package_version" "example2" {
-  name      = "foo"
-  enable = true
-  category  = data.opslevel_rubric_category.security.id
-  level     = data.opslevel_rubric_level.bronze.id
-  notes     = "Optional additional info on why this check is run or how to fix it"
+  name     = "foo"
+  enable   = true
+  category = data.opslevel_rubric_category.security.id
+  level    = data.opslevel_rubric_level.bronze.id
+  notes    = "Optional additional info on why this check is run or how to fix it"
 
-  package_constraint = "matches-version"
-  package_manager = "npm"
-  package_name = "leftpad"
+  package_constraint     = "matches-version"
+  package_manager        = "npm"
+  package_name           = "leftpad"
   missing_package_result = "passed"
-    version_constraint_predicate = {
-        type = "matches_regex"
-        value = "1.0.*"
-    }
+  version_constraint_predicate = {
+    type  = "matches_regex"
+    value = "1.0.*"
+  }
 }
 
 resource "opslevel_check_package_version" "example3" {
-  name      = "foo"
-  enable = true
-  category  = data.opslevel_rubric_category.security.id
-  level     = data.opslevel_rubric_level.bronze.id
-  notes     = "Optional additional info on why this check is run or how to fix it"
+  name     = "foo"
+  enable   = true
+  category = data.opslevel_rubric_category.security.id
+  level    = data.opslevel_rubric_level.bronze.id
+  notes    = "Optional additional info on why this check is run or how to fix it"
 
-  package_constraint = "matches-version"
-  package_manager = "go"
-  package_name = "client-go/.*"
-  pakcage_name_is_regex = true
+  package_constraint     = "matches-version"
+  package_manager        = "go"
+  package_name           = "client-go/.*"
+  pakcage_name_is_regex  = true
   missing_package_result = "passed"
   version_constraint_predicate = {
-    type = "matches_regex"
+    type  = "matches_regex"
     value = "1.27.*"
   }
 }
