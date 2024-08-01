9efeb6d4a461726bd40f004444dfdcfece0ea670
Rocktavious <rocktavious@icloud.com>
2024-08-01 12:43:20 -0500

    Add custom property check

--- examples/resources/opslevel_check_service_property/resource.tf
+++ examples/resources/opslevel_check_service_property/resource.tf
@@ -39,3 +39,19 @@ resource "opslevel_check_service_property" "example" {
   }
   notes = "Optional additional info on why this check is run or how to fix it"
 }
+
+resource "opslevel_check_service_property" "example2" {
+  name    = "foo2"
+  enabled = true
+  # To set a future enable date remove field 'enabled' and use 'enable_on'
+  # enable_on = "2022-05-23T14:14:18.782000Z"
+  category            = data.opslevel_rubric_category.security.id
+  level               = data.opslevel_rubric_level.bronze.id
+  property            = "custom_property"
+  property_definition = "my_custom_property"
+  predicate = {
+    type  = "equals"
+    value = "foo"
+  }
+  notes = "Optional additional info on why this check is run or how to fix it"
+}
