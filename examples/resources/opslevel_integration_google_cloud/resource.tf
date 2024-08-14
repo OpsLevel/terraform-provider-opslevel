699f8d89a123323e60eeb57c9304cecc218cd823
Taimoor Ahmad <taimoor@opslevel.com>
2024-08-14 15:07:22 -0400

    add local tests

---
+++ examples/resources/opslevel_integration_google_cloud/resource.tf
@@ -0,0 +1,11 @@
+resource "opslevel_integration_google_cloud" "dev" {
+  client_email            = "hello-world-tf@powerful-surf-427415-v1.iam.gserviceaccount.com"
+  name                    = "GCP Dev Integration"
+  ownership_tag_keys      = toset(["opslevel_team", "team", "owner"])
+  ownership_tag_overrides = true
+  private_key             = <<EOH
+-----BEGIN PRIVATE KEY-----
+<private key body here>
+-----END PRIVATE KEY-----
+EOH
+}
