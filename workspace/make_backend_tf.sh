d628986a5bb7310bde471d1f3d0a93fde9b43d74
Rocktavious <rocktavious@icloud.com>
2024-08-15 21:12:36 -0500

    I’m not sure why we did this but this is way easier to work with this way

--- workspace/make_backend_tf.sh
+++ workspace/make_backend_tf.sh
@@ -11,23 +11,5 @@ terraform {
 }
 
 provider "opslevel" {
-  api_url     = var.OPSLEVEL_API_URL
-  api_token   = var.OPSLEVEL_API_TOKEN
-  api_timeout = var.OPSLEVEL_API_TIMEOUT
-}
-
-variable "OPSLEVEL_API_URL" {
-  type    = string
-  default = "https://app.opslevel.com"
-}
-
-variable "OPSLEVEL_API_TOKEN" {
-  type      = string
-  sensitive = true
-}
-
-variable "OPSLEVEL_API_TIMEOUT" {
-  type    = number
-  default = 10
 }
 EOF
