c0a411e9fe0c79e4215f8469809b26ee41a9f955
David Bloss <david@opslevel.com>
2024-08-28 09:22:15 -0700

    opslevel_property_assignment tests added (#459)
    * opslevel_property_assignment tests added
    
    * link to ticket in comment about bug

---
+++ tests/remote/property_assignment/main.tf
@@ -0,0 +1,5 @@
+resource "opslevel_property_assignment" "test" {
+  definition = var.definition
+  owner      = var.owner
+  value      = var.value
+}
--- tests/remote/property_definition/variables.tf
+++ tests/remote/property_definition/variables.tf
@@ -6,6 +6,7 @@ variable "allowed_in_config_files" {
 variable "description" {
   type        = string
   description = "The description of the property definition."
+  default     = null
 }
 
 variable "name" {
@@ -16,22 +17,9 @@ variable "name" {
 variable "property_display_status" {
   type        = string
   description = "The display status of a custom property on service pages."
-
-  validation {
-    condition = var.property_display_status == null ? true : contains([
-      "hidden",
-      "visible",
-    ], var.property_display_status)
-    error_message = "expected property_display_status to be 'hidden' or 'visible'"
-  }
 }
 
 variable "schema" {
   type        = string
   description = "The schema of the property definition."
-
-  validation {
-    condition     = var.schema == null ? true : can(jsondecode(var.schema))
-    error_message = "expected schema to be valid JSON"
-  }
 }
