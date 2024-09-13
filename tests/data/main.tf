83a38b5592fb0fbce8033c84851743f624de1168
David Bloss <david@opslevel.com>
2024-09-13 15:55:46 -0500

    WIP: refactor filter integration tests part 1

---
+++ tests/data/main.tf
@@ -0,0 +1,13 @@
+data "opslevel_domains" "all" {}
+
+data "opslevel_filters" "all" {}
+
+# NOTE: below part of future work
+
+# data "opslevel_repositories" "all" {}
+
+# data "opslevel_services" "all" {}
+
+# data "opslevel_teams" "all" {}
+
+# data "opslevel_users" "all" {}
---
+++ tests/data/outputs.tf
@@ -0,0 +1,49 @@
+output "all_domains" {
+  value = data.opslevel_domains.all
+}
+
+output "all_filters" {
+  value = data.opslevel_filters.all
+}
+
+# NOTE: below part of future work
+
+# output "all_repositories" {
+#   value = data.opslevel_repositories.all
+# }
+
+# output "all_services" {
+#   value = data.opslevel_services.all
+# }
+
+# output "all_teams" {
+#   value = data.opslevel_teams.all
+# }
+
+# output "all_users" {
+#   value = data.opslevel_users.all
+# }
+
+output "first_domain" {
+  value = data.opslevel_domains.all.domains[0]
+}
+
+output "first_filter" {
+  value = data.opslevel_filters.all.filters[0]
+}
+
+# output "first_repository" {
+#   value = data.opslevel_repositories.all.repositories[0]
+# }
+
+# output "first_service" {
+#   value = data.opslevel_services.all.services[0]
+# }
+
+# output "first_team" {
+#   value = data.opslevel_teams.all.teams[0]
+# }
+
+# output "first_user" {
+#   value = data.opslevel_users.all.users[0]
+# }
