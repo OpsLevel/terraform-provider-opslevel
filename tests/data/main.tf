4969137a9fd2367c9d1a51cd06dd3c3213365b0a
David Bloss <david@opslevel.com>
2024-09-16 09:39:01 -0500

    uncomment datasources

--- tests/data/main.tf
+++ tests/data/main.tf
@@ -2,12 +2,10 @@ data "opslevel_domains" "all" {}
 
 data "opslevel_filters" "all" {}
 
-# NOTE: below part of future work
+data "opslevel_repositories" "all" {}
 
-# data "opslevel_repositories" "all" {}
+data "opslevel_services" "all" {}
 
-# data "opslevel_services" "all" {}
+data "opslevel_teams" "all" {}
 
-# data "opslevel_teams" "all" {}
-
-# data "opslevel_users" "all" {}
+data "opslevel_users" "all" {}
--- tests/data/outputs.tf
+++ tests/data/outputs.tf
@@ -6,23 +6,21 @@ output "all_filters" {
   value = data.opslevel_filters.all
 }
 
-# NOTE: below part of future work
+output "all_repositories" {
+  value = data.opslevel_repositories.all
+}
 
-# output "all_repositories" {
-#   value = data.opslevel_repositories.all
-# }
+output "all_services" {
+  value = data.opslevel_services.all
+}
 
-# output "all_services" {
-#   value = data.opslevel_services.all
-# }
+output "all_teams" {
+  value = data.opslevel_teams.all
+}
 
-# output "all_teams" {
-#   value = data.opslevel_teams.all
-# }
-
-# output "all_users" {
-#   value = data.opslevel_users.all
-# }
+output "all_users" {
+  value = data.opslevel_users.all
+}
 
 output "first_domain" {
   value = data.opslevel_domains.all.domains[0]
@@ -32,18 +30,18 @@ output "first_filter" {
   value = data.opslevel_filters.all.filters[0]
 }
 
-# output "first_repository" {
-#   value = data.opslevel_repositories.all.repositories[0]
-# }
+output "first_repository" {
+  value = data.opslevel_repositories.all.repositories[0]
+}
 
-# output "first_service" {
-#   value = data.opslevel_services.all.services[0]
-# }
+output "first_service" {
+  value = data.opslevel_services.all.services[0]
+}
 
-# output "first_team" {
-#   value = data.opslevel_teams.all.teams[0]
-# }
+output "first_team" {
+  value = data.opslevel_teams.all.teams[0]
+}
 
-# output "first_user" {
-#   value = data.opslevel_users.all.users[0]
-# }
+output "first_user" {
+  value = data.opslevel_users.all.users[0]
+}
