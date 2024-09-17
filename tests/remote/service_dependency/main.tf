917b7f8805961684001e6ff68483e15699a04e03
David Bloss <david@opslevel.com>
2024-09-17 09:48:04 -0500

    rm unused service related test modules

--- tests/remote/service_dependency/main.tf
+++
@@ -1,5 +0,0 @@
-resource "opslevel_service_dependency" "test" {
-  depends_upon = var.depends_upon
-  note         = var.note
-  service      = var.service
-}
--- tests/remote/service_repository/main.tf
+++
@@ -1,8 +0,0 @@
-resource "opslevel_service_repository" "test" {
-  base_directory   = var.base_directory
-  name             = var.name
-  repository       = var.repository
-  repository_alias = var.repository_alias
-  service          = var.service
-  service_alias    = var.service_alias
-}
