2b168b13060ae83c2f1a8d8bfb7047113c64ecc9
David Bloss <david@opslevel.com>
2024-10-04 15:59:50 -0500

    add opslevel_integration_custom_event_check resource

---
+++ examples/resources/opslevel_integration_custom_event_check/import.sh
@@ -0,0 +1,1 @@
+terraform import opslevel_integration_custom_event_check.example Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS82MDI0
---
+++ examples/resources/opslevel_integration_custom_event_check/resource.tf
@@ -0,0 +1,154 @@
+resource "opslevel_integration_custom_event_check" "apiDoc" {
+  name = "apiDoc custom event check integration"
+  type = "apiDoc"
+}
+
+resource "opslevel_integration_custom_event_check" "aquaSecurity" {
+  name = "aquaSecurity custom event check integration"
+  type = "aquaSecurity"
+}
+
+resource "opslevel_integration_custom_event_check" "argocd" {
+  name = "argocd custom event check integration"
+  type = "argocd"
+}
+
+resource "opslevel_integration_custom_event_check" "awsEcr" {
+  name = "awsEcr custom event check integration"
+  type = "awsEcr"
+}
+
+resource "opslevel_integration_custom_event_check" "bugsnag" {
+  name = "bugsnag custom event check integration"
+  type = "bugsnag"
+}
+
+resource "opslevel_integration_custom_event_check" "circleci" {
+  name = "circleci custom event check integration"
+  type = "circleci"
+}
+
+resource "opslevel_integration_custom_event_check" "codacy" {
+  name = "codacy custom event check integration"
+  type = "codacy"
+}
+
+resource "opslevel_integration_custom_event_check" "coveralls" {
+  name = "coveralls custom event check integration"
+  type = "coveralls"
+}
+
+resource "opslevel_integration_custom_event_check" "customEvent" {
+  name = "customEvent custom event check integration"
+  type = "customEvent"
+}
+
+resource "opslevel_integration_custom_event_check" "datadogCheck" {
+  name = "datadogCheck custom event check integration"
+  type = "datadogCheck"
+}
+
+resource "opslevel_integration_custom_event_check" "deploy" {
+  name = "deploy custom event check integration"
+  type = "deploy"
+}
+
+resource "opslevel_integration_custom_event_check" "dynatrace" {
+  name = "dynatrace custom event check integration"
+  type = "dynatrace"
+}
+
+resource "opslevel_integration_custom_event_check" "flux" {
+  name = "flux custom event check integration"
+  type = "flux"
+}
+
+resource "opslevel_integration_custom_event_check" "jenkins" {
+  name = "jenkins custom event check integration"
+  type = "jenkins"
+}
+
+resource "opslevel_integration_custom_event_check" "jfrogXray" {
+  name = "jfrogXray custom event check integration"
+  type = "jfrogXray"
+}
+
+resource "opslevel_integration_custom_event_check" "githubActions" {
+  name = "githubActions custom event check integration"
+  type = "githubActions"
+}
+
+resource "opslevel_integration_custom_event_check" "gitlabCi" {
+  name = "gitlabCi custom event check integration"
+  type = "gitlabCi"
+}
+
+resource "opslevel_integration_custom_event_check" "grafana" {
+  name = "grafana custom event check integration"
+  type = "grafana"
+}
+
+resource "opslevel_integration_custom_event_check" "grype" {
+  name = "grype custom event check integration"
+  type = "grype"
+}
+
+resource "opslevel_integration_custom_event_check" "lacework" {
+  name = "lacework custom event check integration"
+  type = "lacework"
+}
+
+resource "opslevel_integration_custom_event_check" "newRelicCheck" {
+  name = "newRelicCheck custom event check integration"
+  type = "newRelicCheck"
+}
+
+resource "opslevel_integration_custom_event_check" "octopus" {
+  name = "octopus custom event check integration"
+  type = "octopus"
+}
+
+resource "opslevel_integration_custom_event_check" "prismaCloud" {
+  name = "prismaCloud custom event check integration"
+  type = "prismaCloud"
+}
+
+resource "opslevel_integration_custom_event_check" "prometheus" {
+  name = "prometheus custom event check integration"
+  type = "prometheus"
+}
+
+resource "opslevel_integration_custom_event_check" "rollbar" {
+  name = "rollbar custom event check integration"
+  type = "rollbar"
+}
+
+resource "opslevel_integration_custom_event_check" "sentry" {
+  name = "sentry custom event check integration"
+  type = "sentry"
+}
+
+resource "opslevel_integration_custom_event_check" "snyk" {
+  name = "snyk custom event check integration"
+  type = "snyk"
+}
+
+resource "opslevel_integration_custom_event_check" "sonarqube" {
+  name = "sonarqube custom event check integration"
+  type = "sonarqube"
+}
+
+resource "opslevel_integration_custom_event_check" "stackhawk" {
+  name = "stackhawk custom event check integration"
+  type = "stackhawk"
+}
+
+resource "opslevel_integration_custom_event_check" "sumoLogic" {
+  name = "sumoLogic custom event check integration"
+  type = "sumoLogic"
+}
+
+resource "opslevel_integration_custom_event_check" "veracode" {
+  name = "veracode custom event check integration"
+  type = "veracode"
+}
