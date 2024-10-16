ba3ea04d928ec5289564f6050b195cd6d1af515a
David Bloss <david@opslevel.com>
2024-10-16 13:21:29 -0500

    provisioner module allows deleting resources mid test, merge team test files

---
+++ tests/provisioner/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_id" {
+  type        = string
+  description = "The ID of the resource to delete."
+}
+
+variable "resource_type" {
+  type        = string
+  description = "The type of the resource to delete."
+}
