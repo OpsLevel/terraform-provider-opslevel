a7974485bb662fd89bf5b4d8b17adb7fb6cda85e
Rocktavious <rocktavious@icloud.com>
2024-10-22 09:48:47 -0500

    Add implementation for managing aliases

--- examples/resources/opslevel_alias/resource.tf
+++ examples/resources/opslevel_alias/resource.tf
@@ -1,40 +1,40 @@
 resource "opslevel_alias" "service" {
-  resource_type       = "Service"
+  resource_type       = "service"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
 }
 
 resource "opslevel_alias" "team" {
-  resource_type       = "Team"
+  resource_type       = "team"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
 }
 
 resource "opslevel_alias" "domain" {
-  resource_type       = "Domain"
+  resource_type       = "domain"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
 }
 
 resource "opslevel_alias" "system" {
-  resource_type       = "System"
+  resource_type       = "system"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
 }
 
 resource "opslevel_alias" "infra" {
-  resource_type       = "Infrastructure_Resource"
+  resource_type       = "infrastructure_resource"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
 }
 
 resource "opslevel_alias" "scorecard" {
-  resource_type       = "Scorecard"
+  resource_type       = "scorecard"
   resource_identifier = "example_alias"
 
   aliases = ["example_alias_2", "example_alias_3"]
---
+++ opslevel/helpers.go
@@ -0,0 +1,16 @@
+package opslevel
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+)
+
+type TerraformSource interface {
+	Get(ctx context.Context, target interface{}) diag.Diagnostics
+}
+
+func read[T any](ctx context.Context, d *diag.Diagnostics, state TerraformSource) T {
+	var data T
+	d.Append(state.Get(ctx, &data)...)
+	return data
+}
