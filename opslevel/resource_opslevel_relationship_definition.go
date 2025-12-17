169fa745815b9b0c79810aa1daf613a30e7827c1
Andrew Still <andrew.still@opslevel.com>
2025-12-17 13:15:08 -0500

    validator on relationship definition

--- opslevel/resource_opslevel_relationship_definition.go
+++ opslevel/resource_opslevel_relationship_definition.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -109,7 +108,7 @@ func (r *RelationshipDefinitionResource) Schema(ctx context.Context, req resourc
 					listvalidator.AtLeastOneOf(path.MatchRoot("allowed_types")),
 				},
 				PlanModifiers: []planmodifier.List{
-					listplanmodifier.RequiresReplace(),
+					PreventRemovalPlanModifier("allowed_categories"),
 				},
 			},
 			"allowed_types": schema.ListAttribute{
@@ -120,7 +119,7 @@ func (r *RelationshipDefinitionResource) Schema(ctx context.Context, req resourc
 					listvalidator.AtLeastOneOf(path.MatchRoot("allowed_categories")),
 				},
 				PlanModifiers: []planmodifier.List{
-					listplanmodifier.RequiresReplace(),
+					PreventRemovalPlanModifier("allowed_types"),
 				},
 			},
 			"management_rules": ManagementRulesResourceAttribute(),
